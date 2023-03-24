6508abde309b72428b43370d9b56608e4b7315e9
rafaelalyns <127249548+rafaelalyns@users.noreply.github.com>
2023-03-24 15:06:34 -0300

    Add files via upload

--- demo/src/test/java/TestCases/RealizarCompraComSucessoTest.java
+++ demo/src/test/java/TestCases/RealizarCompraComSucessoTest.java
@@ -5,15 +5,19 @@ import org.openqa.selenium.WebDriver;
 
 import Framework.TestBase;
 import Tasks.LoginTask;
+import Tasks.ProdutoTask;
 
 public class RealizarCompraComSucessoTest extends TestBase {
      
     private WebDriver driver = getDriverManager();
 LoginTask loginTask = new LoginTask(driver);
+ProdutoTask produtoTask = new ProdutoTask(driver);
 
 @Test
 public void realizarCompra() {
     loginTask.efetuarLogin();
+    produtoTask.selecionarProduto();
+    
 }
 
 }
